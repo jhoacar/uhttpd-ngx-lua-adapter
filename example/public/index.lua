0cde2ccf37f27ee4d9c926ff3c3026cc17f80986
jhoacar <carrerojhoan@gmail.com>
2022-08-22 02:06:20 -0300

    added working example

--- example/public/index.lua
+++ example/public/index.lua
@@ -1,15 +1,13 @@
 #!/usr/bin/lua
 
--- local lapis = require("lapis")
+local lapis = require("lapis")
 
 require("nginx.uhttpd.adapter")
 
-ngx.print("Hello world!")
+local app = lapis.Application()
 
--- local app = lapis.Application()
+app:match("/", function(self)
+    return "Hello world!"
+end)
 
--- app:match("/", function(self)
---     return "Hello world!"
--- end)
-
--- lapis.serve(app)
+lapis.serve(app)
